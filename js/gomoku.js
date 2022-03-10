d51e9da43d615902f40d76a6d289b604845944f7
helloShen <symantec__@hotmail.com>
2022-03-10 01:38:05 -0500

    v1.0: first official minimum version

--- js/gomoku.js
+++ js/gomoku.js
@@ -35,6 +35,30 @@ const board = (() => {
                 frontLayer.appendChild(grid);
             }
         }
+        initPhantomPiece();
+        initRealPiece();
+    }
+
+    function initPhantomPiece() {
+        frontLayer.querySelectorAll('.grid').forEach(grid => {
+            grid.addEventListener('mouseenter', (e) => {
+                e.stopPropagation();
+                players.getCurrentPlayer().placePhantomPiece(grid);
+            }, false);
+            grid.addEventListener('mouseleave', (e) => {
+                e.stopPropagation();
+                players.getCurrentPlayer().removePhantomPiece(grid);
+            }, false);
+        });
+    }
+
+    function initRealPiece() {
+        frontLayer.querySelectorAll('.grid').forEach(grid => {
+            grid.addEventListener('click', (e) => {
+                e.stopPropagation();
+                players.getCurrentPlayer().placeRealPiece(grid);
+            }, false);
+        });
     }
 
     function draw(size) {
@@ -45,11 +69,10 @@ const board = (() => {
         board.appendChild(backLayer);
         drawFrontLayer(size);
         board.appendChild(frontLayer);
-        return board;
     }
 
     function getBoard() {
-        return frontLayer;
+        return board;
     }
 
     function gridIsEmpty(grid) {
@@ -78,8 +101,13 @@ const board = (() => {
 
 /* player factory function 
  * piece is the path to the piece image */
-const player = function(pieceImg) {
+const player = function(playerName, pieceImg) {
+    const name = playerName;
     const piece = pieceImg;
+
+    function getName() {
+        return name;
+    }
 
     function createPiece() {
         const newPiece = document.createElement('img');
@@ -115,7 +143,11 @@ const player = function(pieceImg) {
             const newPiece = createRealPiece.call(this);
             removePhantomPiece.call(this, grid);
             grid.appendChild(newPiece);
-            gomoku.cachePieces(grid);
+            addIntoPieceMap.call(this, grid);
+            game.cachePieces(grid);
+            if (checkWin.call(this)) {
+                game.announceWinner(this);
+            }
             players.alternatePlayer();
         }
     }
@@ -123,16 +155,74 @@ const player = function(pieceImg) {
     function removeRealPiece(grid) {
         const piece = grid.querySelector('img');
         if (piece) grid.removeChild(piece);
-        players.alternatePlayer();
+        removeFromPieceMap.call(this, grid);
     }
 
-    return { placePhantomPiece, removePhantomPiece, placeRealPiece, removeRealPiece };
+    function initPieceMap(size) {
+        this.pieceMap = [];
+        this.pieceMapSize = size;
+        for (let row = 0; row < size; row++) {
+            this.pieceMap.push([]);
+            for (let col = 0; col < size; col++) {
+                this.pieceMap[row].push(0);
+            }
+        }
+    }
+
+    function addIntoPieceMap(grid) {
+        this.pieceMap[grid.dataset.row][grid.dataset.col] = 1;
+    }
+
+    function removeFromPieceMap(grid) {
+        this.pieceMap[grid.dataset.row][grid.dataset.col] = 0;
+    }
+
+    function checkWin() {
+        const rowCount = [];
+        const colCount = [];
+        for (let i = 0; i < this.pieceMapSize; i++) {
+            rowCount.push(0);
+            colCount.push(0);
+        }
+        const diagonalCount = [];
+        const antiDiagonalCount = [];
+        for (let i = 0; i < this.pieceMapSize * 2 - 1; i++) {
+            diagonalCount.push(0);
+            antiDiagonalCount.push(0);
+        }
+        for (let row = 0; row < this.pieceMapSize; row++) {
+            for (let col = 0; col < this.pieceMapSize; col++) {
+                const diagonalIdx = row - col + this.pieceMapSize - 1;
+                const antiDiagonalIdx = row + col;
+                if (this.pieceMap[row][col] === 1) {
+                    rowCount[row]++;
+                    colCount[col]++;
+                    diagonalCount[diagonalIdx]++;
+                    antiDiagonalCount[antiDiagonalIdx]++;
+                    if (rowCount[row] === 5 || 
+                        colCount[col] === 5 || 
+                        diagonalCount[diagonalIdx] === 5 || 
+                        antiDiagonalCount[antiDiagonalIdx] === 5) {
+                        return true;
+                    }
+                } else {
+                    rowCount[row] = 0;
+                    colCount[col] = 0;
+                    diagonalCount[diagonalIdx] = 0;
+                    antiDiagonalCount[antiDiagonalIdx] = 0;
+                }
+            }
+        }
+        return false;
+    }
+
+    return { getName, placePhantomPiece, removePhantomPiece, placeRealPiece, removeRealPiece, initPieceMap, checkWin };
 };
 
 const players = (() => {
     const blackPiece = './assets/img/black_piece.svg';
     const whitePiece = './assets/img/white_piece.svg';
-    const players = [player(blackPiece), player(whitePiece)]; 
+    const players = [player('Black', blackPiece), player('White', whitePiece)]; 
     let currentPlayer = 0;
 
     function alternatePlayer() {
@@ -143,52 +233,48 @@ const players = (() => {
         return players[currentPlayer];
     }
 
-    return { alternatePlayer, getCurrentPlayer };
+    function initPieceMap(size) {
+        players.forEach(target => target.initPieceMap(size));
+    }
+
+    return { alternatePlayer, getCurrentPlayer, initPieceMap };
 })();
 
 const controllers = (() => {
     const rollBackController = createRollBackController();
+    const restartController = createRestartController();
 
     function createRollBackController() {
         const btn = document.createElement('button');
         btn.classList.add('rollBackBtn');
         btn.textContent = 'Back';
-        btn.addEventListener('click', () => gomoku.rollBack());
+        btn.addEventListener('click', () => game.rollBack());
         return btn;
     }
 
-    return { rollBackController };
+    function createRestartController() {
+        const btn = document.createElement('button');
+        btn.classList.add('restartBtn');
+        btn.textContent = 'Restart';
+        btn.addEventListener('click', () => game.restart());
+        return btn;
+    }
+
+    return { rollBackController, restartController };
 })();
 
-export const gomoku = (() => {
-    
-    const pieces = [];
-    let id = 0;
+const game = (() => {
+
+    let pieces = undefined;
+    let id = undefined;
+
+    function initPiecesCache() {
+        pieces = [];
+        id = 0;
+    }
 
     function nextId() {
         return ++id;
-    }
-
-    function initPhantomPiece() {
-        board.getBoard().querySelectorAll('.grid').forEach(grid => {
-            grid.addEventListener('mouseenter', (e) => {
-                e.stopPropagation();
-                players.getCurrentPlayer().placePhantomPiece(grid);
-            }, false);
-            grid.addEventListener('mouseleave', (e) => {
-                e.stopPropagation();
-                players.getCurrentPlayer().removePhantomPiece(grid);
-            }, false);
-        });
-    }
-
-    function initRealPiece() {
-        board.getBoard().querySelectorAll('.grid').forEach(grid => {
-            grid.addEventListener('click', (e) => {
-                e.stopPropagation();
-                players.getCurrentPlayer().placeRealPiece(grid);
-            }, false);
-        });
     }
 
     function cachePieces(grid) {
@@ -199,21 +285,42 @@ export const gomoku = (() => {
         const lastPiece = pieces.pop();
         if (lastPiece) {
             const grid = board.getGrid(lastPiece.row, lastPiece.col);
+            players.alternatePlayer();
             players.getCurrentPlayer().removeRealPiece(grid);
         }
     }
 
-    function start() {
-        initPhantomPiece();
-        initRealPiece();
+    function restart() {
+        while (pieces.length > 0) rollBack();
     }
 
-    function initIn(size, container) {
-        container.appendChild(controllers.rollBackController);
-        container.appendChild(board.draw(size));
-        start();
+    function announceWinner(winner) {
+        alert(`${winner.getName()} win!`);
     }
-        
-    return { initIn, cachePieces, rollBack };
+
+    return { initPiecesCache, cachePieces, rollBack, restart, announceWinner };
 })();
 
+
+
+/* API */
+export default (() => {
+    function initModules(size) {
+        board.draw(size);
+        players.initPieceMap(size);
+        game.initPiecesCache();
+    }
+
+    function insertToWebPage(container) {
+        container.appendChild(controllers.rollBackController);
+        container.appendChild(controllers.restartController);
+        container.appendChild(board.getBoard());
+    }
+
+    function init(size, container) {
+        initModules(size);        
+        insertToWebPage(container);        
+    }
+
+    return { init };
+})();
--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-import { gomoku } from './gomoku.js';
+import gomoku from './gomoku.js';
 
-
-gomoku.initIn(15, document.querySelector('body'));
-// document.querySelector('button').addEventListener('click', () => gomoku.rollBack());
+gomoku.init(15, document.querySelector('body'));
